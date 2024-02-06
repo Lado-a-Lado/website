70e5456c0f62152277a4118f1b844baee5ec001f
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2024-02-06 00:10:16 +0000

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<main>
+    <h1 id=title>Lado a Lado</h1>
+</main>
+
+<p>Olá, mãe!</p>
+
+<style lang="scss">
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background-color: #FFE681;
+    background-image: radial-gradient(at top right, #6D2E45, transparent 10%), radial-gradient(at bottom left, #6D2E45, transparent 10%);
+  }
+
+  #title {
+    font-face: 'Open Sans', sans-serif;
+    font-weight: bold;
+    color: #BD4747;
+  }
+</style>
