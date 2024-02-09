4f6728aae6e6ae61a51ba9575ab29135a741e5f9
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2024-02-09 14:41:02 +0000

    headering

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,14 +1,42 @@
-<nav>
-    <a href="/">~Lado a Lado ~</a>
+<script>
+  import {currentIsLanding} from "$lib";
+</script>
 
-    <ul>
-        <li><a href="/quem-somos">Quem somos</a></li>
-        <li><a href="/blogue">Blogue</a></li>
-    </ul>
+<nav>
+    <a class="logo" href><img src="/img/logo-name.svg" alt="Lado a Lado"/></a>
+
+    <div class="links" class:be-at-bottom={$currentIsLanding}>
+        <a href="/quem-somos">Quem somos</a>
+        <a href="/blogue">Blogue</a>
+    </div>
 </nav>
 
-<style>
-    ul {
-        display: flex;
+<style lang="scss">
+  nav {
+    position: fixed;
+    width: 100vw;
+  }
+
+  .logo {
+    display: block;
+    width: 210px;
+    margin: 30px;
+
+    img {
+      width: 100%;
     }
+  }
+
+  .links {
+    display: flex;
+    gap: 20px;
+    position: absolute;
+    top: 33%;
+    left: 50%;
+    translate: -50%;
+    font-size: 1.2em;
+
+    &.be-at-bottom {
+    }
+  }
 </style>
