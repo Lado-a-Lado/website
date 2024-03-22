da5edf6e4b7cbcebd821def5f7a9388eb01d9642
luis <72473883+LuisTovar0@users.noreply.github.com>
2024-03-22 14:37:43 +0000

    desktop improvement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,18 +7,16 @@
     <!--    </div>-->
 </div>
 
-<section id=landing-section class="relative overflow-hidden h-svh max-h-[700px] sm:max-h-[900px] md:max-h-[1000px] min-h-[750px]
+<section id=landing-section class="relative overflow-hidden h-svh max-h-[950px] sm:min-h-[900px] sm:max-h-[1000px] md:max-h-[1080px] min-h-[750px]
  flex flex-col justify-center gap-4 bg-cover">
     <div class="absolute top-[55%] sm:top-2/3 md:top-[60%] md:-translate-y-1/2 left-1/2 -translate-x-1/2 md:left-1/3 w-80 md:w-[600px]">
         <div class="color-red mono flex gap-2 items-center font-bold">
-            <span class="text-lg md:text-3xl">CONFERÊNCIA</span>
-            <span class="text-sm md:text-2xl">&bullet; 3 MARÇO 2024, LISBOA</span>
+            <span class="text-lg md:text-3xl">EVENTO</span>
+            <span class="text-sm md:text-2xl">&bullet; 6 ABRIL 2024, LISBOA</span>
         </div>
-        <h1 class="fruits-delight text-black my-2 md:my-6 text-[45px] md:text-7xl">Viver para amar</h1>
-        <h2 class="rubik md:text-xl">Um evento sobre</h2>
-        <button class="sign-up my-16 text-xl md:text-2xl">
-            INSCREVE-TE AQUI!
-        </button>
+        <h1 class="fruits-delight text-black my-2 md:my-6 text-[45px] md:text-7xl">Feitos para amar</h1>
+        <h2 class="rubik md:text-xl">LAL Talks</h2>
+        <button class="sign-up my-16 text-xl md:text-2xl"> RESERVA GRÁTIS AQUI </button>
     </div>
 
     <img class="huge-logo absolute top-20 md:top-0 -right-[25%] w-[120%] md:w-full max-w-none max-h-[40%] md:max-h-[80%]"
@@ -28,7 +26,7 @@
 <section id="sobre" class="h-svh min-h-96 md:h-auto md:gap-4 md:py-12 flex flex-col items-center justify-evenly">
     <h1 class="color-red mono">SOBRE</h1>
     <h2 class="fruits-delight text-3xl md:text-4xl xl:text-5xl">Em que consiste?</h2>
-    <div class="md:pt-6 flex flex-col md:flex-row items-center justify-evenly gap-16 w-11/12">
+    <div class="flex flex-col md:flex-row items-center gap-16 w-11/12 my-8 md:my-16">
         <div class="w-full md:w-9/12">
             <div class="bg-black aspect-video"><span class="text-white w-full h-full items-center flex justify-center">
                 Video</span></div>
