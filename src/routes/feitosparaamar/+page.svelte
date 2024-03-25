f2576c77af4913f1e9f36c62f1a06d60514d1278
luis <72473883+LuisTovar0@users.noreply.github.com>
2024-03-25 21:40:36 +0000

    sobre

--- src/routes/feitosparaamar/+page.svelte
+++ src/routes/feitosparaamar/+page.svelte
@@ -38,7 +38,7 @@
          src="/img/logo-hands.svg" alt="Desenho de mãos dadas"/>
 </section>
 
-<section class="h-[800px] md:max-h-none md:h-auto md:gap-4 md:py-12 flex flex-col items-center justify-evenly">
+<section class="h-[700px] md:max-h-none md:h-auto md:gap-4 md:py-12 flex flex-col items-center justify-evenly">
     <h1 class="color-red mono">SOBRE</h1>
     <h2 class="fruits-delight text-3xl md:text-4xl xl:text-5xl">Em que consiste?</h2>
     <div class="flex flex-col md:flex-row items-center justify-center gap-16 w-11/12 my-8 md:my-16">
@@ -47,18 +47,23 @@
         <!--                Video</span></div>-->
         <!--        </div>-->
         <div class="explanation w-full md:w-[700px]">
-            <p class="my-4">Todos sabemos quanto um voluntariado, uma Missão País nos enche.
-                <br/> Todos já sentimos borboletas na barriga por alguém.
-                <br/> Por que é tão bom amar alguém?</p>
+            <p class="my-4">
+                <br/>Todos já sentimos borboletas na barriga.
+                <br/>Todos sabemos quanto um voluntariado nos enche.
+                <br/>Por que é tão bom amar alguém?</p>
 
-            <p class="my-4"> O que é que estas três frases têm em comum?
-                <br/> Porque nos alteram tanto?</p>
+            <p class="my-4">
+                <br/>O que é que estas três frases têm em comum?
+                <br/>Porque nos alteram tanto?</p>
 
-            <p class="my-4"> Nesta conferência queremos percorrer um caminho que vai desde a raiz destas questões até à
-                forma como é possível viver uma vida mais cheia de sentido.
+            <p class="my-4">
+                <br/>Nesta conferência queremos percorrer um caminho que vai desde a raiz destas questões até à forma
+                como é possível viver uma vida mais cheia de sentido.
                 <br/> Vais ficar de fora?
-                <br/> Ou queres descobrir para que fomos feitos?</p>
-            <p>Vemo-nos dia 6?</p>
+                <br/> Ou queres descobrir para que fomos feitos?
+            </p>
+
+            <p class="my-4">Inscreve-te!</p>
         </div>
     </div>
     <a href=https://tally.so/r/nGzJre type=button target=_blank class="sign-up-button block md:hidden self-start ml-5">INSCRIÇÃO
@@ -76,7 +81,7 @@
     <h2 class="fruits-delight text-center text-3xl md:text-4xl xl:text-5xl">Que programa temos para oferecer?</h2>
     <div class="horário flex flex-col w-11/12 max-w-[900px]">
         <div class="color-red mono !items-end">
-            <span class="text-lg font-semibold">SÁBADO, 6 ABRIL</span>
+            <span class="text-lg font-semibold w-2/3">SÁBADO, 6 ABRIL</span>
             <div class="flex flex-col items-end">
                 <span>Lisboa</span>
                 <span class="text-right">Universidade Católica</span>
