03d1d11b3bc7fcd7330b9714b71822cb047c9e7b
luis <72473883+LuisTovar0@users.noreply.github.com>
2024-03-26 14:45:59 +0000

    footer done

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,32 +1,50 @@
-<footer class="grid items-center">
+<script>
+  import {vw} from "$lib";
+</script>
 
-    <div class="flex flex-col md:flex-row items-center">
+<footer class="grid items-center p-8 text-white">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8 place-items-center">
         <img src="/img/small-lado-a-lado.svg" alt="Lado a Lado escrito à mão"/>
 
-        <span>2024</span>
+        {#if ($vw >= 768)}
+            <span>2024</span>
+        {/if}
 
-        <div class="flex gap-8 items-center">
+        <div class="flex gap-10 items-center">
             <a href="https://open.spotify.com/show/79y8yNDSivKHovVU28g4oI?si=7f2c649a98df410e">
                 <img src="/img/spoti.svg" alt="Spotify"/>
             </a>
-            <a href="https://www.youtube.com/@lado_a_lado">
+            <a href="https://www.youtube.com/@lado_a_lado" class="yt">
                 <img src="/img/yt.svg" alt="YouTube"/>
             </a>
             <a href="https://www.instagram.com/ladoaladopt">
                 <img src="/img/insta.svg" alt="Instagram"/>
             </a>
         </div>
+
+        {#if ($vw < 768)}
+            <span>2024</span>
+        {/if}
     </div>
 </footer>
 
 <style>
+    :root {
+        --base-dimention: 32px;
+        --dimention: var(--base-dimention);
+    }
+
     footer {
         background-color: var(--theme-brown);
     }
 
     a {
-        --dimention: 48px;
+        @apply flex justify-center items-center;
         height: var(--dimention);
         width: var(--dimention);
+    }
+
+    .yt {
+        --dimention: calc(var(--base-dimention) * 1.15);
     }
 </style>
