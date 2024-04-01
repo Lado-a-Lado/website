75e51bc31f2ee5a67501ce1e638c3d660a629eb1
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2024-04-01 13:39:53 +0000

    replace vercel analytics by google analytics. remove unused font packages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,32 @@
 <script>
+  import { partytownSnippet } from '@builder.io/partytown/integration';
+  import { onMount } from "svelte";
+  import "../styles.scss";
   import Header from "./Header.svelte";
   import Footer from "./Footer.svelte";
-  import "../styles.scss";
-  import {onMount} from "svelte";
-  import {updateVw} from "$lib";
-  import { inject } from '@vercel/analytics'
-  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+  import { updateVw } from "$lib";
 
-  injectSpeedInsights();
-  inject();
   onMount(() => updateVw());
 </script>
+
+<svelte:head>
+	<script>
+		// Forward the necessary functions to the web worker layer
+		partytown = {
+			forward: ['dataLayer.push', 'gtag']
+		};
+	</script>
+
+	{@html '<script>' + partytownSnippet() + '</script>'}
+
+	<script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-RR3N6CW5T9"></script>
+	<script type="text/partytown">
+		window.dataLayer = window.dataLayer || [];
+		window.gtag = function(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-RR3N6CW5T9');
+	</script>
+</svelte:head>
 
 <svelte:window on:resize={updateVw}/>
 
