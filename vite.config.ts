60732ae5955083bfe68f3b7368b05fda5a8bd9d6
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2024-04-01 15:19:38 +0000

    fix GA (?)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { join } from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { partytownVite } from '@builder.io/partytown/utils';
 
 /** @type {import('vite').UserConfig} */
 export default {
   plugins: [
     sveltekit(),
-    partytownVite({
-      dest: join(process.cwd(), 'static', '~partytown'),
-    }),
   ],
 };
