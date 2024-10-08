e910daae5e83fcd28c510103aec214660db35551
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2024-10-08 12:33:30 +0000

    created main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,13 @@
 <script>
-  import Header from "./Header.svelte";
-  import Footer from "./Footer.svelte";
-  import "../styles.scss";
   import {onMount} from "svelte";
+  import "../styles.scss";
+  import Footer from "./Footer.svelte";
   import {updateVw} from "$lib";
 
   onMount(() => updateVw());
 </script>
 
 <svelte:window on:resize={updateVw}/>
-
-<Header/>
 
 <main>
     <slot/>
